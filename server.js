18792886619bb5dfdbbd3a4c0a7fba3a44a7d99e
Christopher <christopherjacobs1234@hotmail.com>
2019-08-26 15:19:23 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+MongoClient.connect('mongodb://Marblehead:8712594630Marble@ds034198.mlab.com:34198/examenexpress', { useNewUrlParser: true },(err, client) =>{
+
+    if (err) return console.log(err)
+    db = client.db('examenexpress') // whatever your database name is
+    app.listen(3000, () => {
+      console.log('listening on 3000')
+})
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine','ejs');
+
