21779b3ba1b6ec874e40a2e33053f71cc4ac2fe6
Christopher <christopherjacobs1234@hotmail.com>
2019-08-26 15:46:01 +0200

    final commit
    Node modules eruit gehaald

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
 var db;
-MongoClient.connect('mongodb://Marblehead:8712594630Marble@ds034198.mlab.com:34198/examenexpress', { useNewUrlParser: true },(err, client) =>{
+MongoClient.connect("mongodb://Marblehead:8712594630Marble@ds034198.mlab.com:34198/examenexpress", { useNewUrlParser: true },(err, client) =>{
 
     if (err) return console.log(err)
     db = client.db('examenexpress') // whatever your database name is
@@ -15,5 +15,19 @@ MongoClient.connect('mongodb://Marblehead:8712594630Marble@ds034198.mlab.com:341
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+
 app.set('view engine','ejs');
+
+
+app.get('/', (req, res) => {
+    db.collection('overtredingen').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      // renders index.ejs
+      res.render('index.ejs', {inhaal: result})
+    })
+  })
+
+
+
+
 
